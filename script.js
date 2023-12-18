70a34138145a1016a4ac86c7a5a8d51b0f6e5801
ScottProuty <proutyscottl@gmail.com>
2023-12-18 14:42:04 -0800

    main game generally working

--- script.js
+++ script.js
@@ -4,77 +4,182 @@
 const scoreElement = document.querySelector('.scoreBox-score');
 const phraseElement = document.querySelector('.phrase');
 const scoreBoxElement = document.querySelector('scoreBox');
-const btnNew = document.querySelector('.btn--new');
+const btnSubmitElement = document.getElementById('btnSubmit');
+const btnDeselectElement = document.getElementById('btnDeselect');
+const cardElements = document.querySelectorAll('.wordCard');
 
 // Variables
 let longTermScore = 0;
 let currentScore = 0;
+let currentPhrase = 0;
+const ptsPerCorrectGuess = 10;
+let cardsList = {};
+let codePhrase = 'No phrase found';
 
 // Starting conditions
 scoreElement.textContent = 0;
-diceElement.classList.add('hidden');
+phraseElement.textContent = codePhrase;
+
+//diceElement.classList.add('hidden');
 
 // Define functions
-
-const holdScore = function () {
-  scores[activePlayer] += currentScore;
-  score0Element.textContent = scores[0];
-  score1Element.textContent = scores[1];
-  if (scores[activePlayer] >= 100) winner(activePlayer);
-  else {
-    const currentPlayerTotalScoreLabel = document.getElementById(
-      `score--${activePlayer}`
-    );
-    const currentPlayerScoreLabel = document.getElementById(
-      `current--${activePlayer}`
-    );
-
-    if (currentScore > 0) {
-      currentPlayerScoreLabel.classList.add('animate-hold');
-
-      currentPlayerTotalScoreLabel.classList.add('addScore');
-      currentPlayerTotalScoreLabel.addEventListener('animationend', () => {
-        currentPlayerTotalScoreLabel.classList.remove('addScore');
-      });
+const deselectAllCards = function () {
+  cardElements.forEach(element => {
+    element.classList.remove('cardSelected');
+  });
+  for (let card in cardsList) {
+    if (cardsList.hasOwnProperty(card)) {
+      cardsList[card].selected = false;
     }
-    switchPlayer();
   }
 };
 
-const winner = function () {
-  btnRoll.disabled = true;
-  btnHold.disabled = true;
-  document.getElementById(`currentLbl--${activePlayer}`).textContent = '👑';
-  document.getElementById(`current--${activePlayer}`).textContent = 'WINNER!';
+function nextPhrase() {
+  deselectAllCards();
+  if (currentPhrase < 2) {
+    currentPhrase++;
+    givePhrase(currentPhrase);
+    console.log(`Advancing to phrase ${currentPhrase}...`);
+  } else {
+    console.log(`Game over! Total score: ${currentScore}`);
+  }
+}
+
+function givePhrase(phraseIndex) {
+  let phrase = Object.keys(gameInfo.phrases)[phraseIndex];
+  // Animate this?
+  phraseElement.textContent = phrase;
+}
+
+function updateCardDisplay() {
+  cardElements.forEach(card => {
+    if (cardsList[card.id].used) {
+      card.classList.add('used');
+    }
+  });
+}
+
+function submit() {
+  let isSelected, isCodeWord, cardWord, isUsed;
+  let scoreToAdd = 0;
+  for (let card in cardsList) {
+    if (cardsList.hasOwnProperty(card)) {
+      isSelected = cardsList[card].selected;
+      isCodeWord = cardsList[card].isCodeWord[currentPhrase];
+      isUsed = cardsList[card].used;
+      cardWord = cardsList[card].word.toUpperCase();
+      if (isSelected && isCodeWord) {
+        // correct selection
+        scoreToAdd += 10 * !isUsed + 20 * isUsed;
+        console.log(`${cardWord} is CORRECT +10pts`);
+      } else if (isSelected && !isCodeWord) {
+        // incorrect selection
+        scoreToAdd -= 7 * !isUsed + 14 * isUsed;
+        console.log(`${cardWord} is INCORRECT -7pts`);
+      } else if (!isSelected && isCodeWord) {
+        // missed a codeWord
+        scoreToAdd -= 3 * !isUsed + 6 * isUsed;
+        console.log(`You missed ${cardWord}, -3pts`);
+      }
+      if (isSelected) {
+        cardsList[card].used = true;
+      }
+    }
+  }
+  updateCardDisplay();
+  addScore(scoreToAdd);
+  nextPhrase();
+}
+
+const clickCard = function () {
+  if (this.classList.contains('cardSelected')) {
+    this.classList.remove('cardSelected');
+    cardsList[this.id].selected = false;
+  } else {
+    this.classList.add('cardSelected');
+    cardsList[this.id].selected = true;
+  }
+};
+
+const addScore = function (numPts) {
+  currentScore += numPts;
+  scoreElement.textContent = currentScore;
+
+  if (numPts > 0) {
+    scoreElement.classList.add('addScore');
+    scoreElement.addEventListener('animationend', () => {
+      scoreElement.classList.remove('addScore');
+    });
+  } else if (numPts < 0) {
+    scoreElement.classList.add('animate-lost');
+    scoreElement.addEventListener('animationend', () => {
+      scoreElement.classList.remove('animate-lost');
+    });
+  }
 };
 
 const resetGame = function () {
-  activePlayer = 0;
-  scores = [0, 0];
   currentScore = 0;
-  document.getElementById(`currentLbl--0`).textContent = 'CURRENT';
-  document.getElementById(`currentLbl--1`).textContent = 'CURRENT';
-  player0Element.classList.add('player--active');
-  player1Element.classList.remove('player--active');
-  btnRoll.disabled = false;
-  btnHold.disabled = false;
-  score0Element.textContent = scores[0];
-  score1Element.textContent = scores[1];
-  current0Element.textContent = 0;
-  current1Element.textContent = 0;
-  current0Element.classList.remove('animate-lost', 'animate-hold');
-  current1Element.classList.remove('animate-lost', 'animate-hold');
+  //disable deselect button
+  scoreElement.textContent = currentScore;
+  populateCardList(wordBank);
+  givePhrase(wordBank.phrase);
 };
 
-const switchPlayer = function () {
-  //document.getElementById(`current--${activePlayer}`).textContent = 0;
-  currentScore = 0;
-  activePlayer = activePlayer === 0 ? 1 : 0;
-  player0Element.classList.toggle('player--active');
-  player1Element.classList.toggle('player--active');
-};
+function populateCardList(gameInfo) {
+  let word = '';
+  for (const [index, cardElement] of cardElements.entries()) {
+    word = gameInfo.words[index];
+    cardElement.textContent = word;
+    cardsList[cardElement.id] = {
+      word: cardElement.textContent,
+      selected: false,
+      disabled: false,
+      used: false,
+      isCodeWord: [
+        gameInfo.phrases[Object.keys(gameInfo.phrases)[0]].includes(word),
+        gameInfo.phrases[Object.keys(gameInfo.phrases)[1]].includes(word),
+        gameInfo.phrases[Object.keys(gameInfo.phrases)[2]].includes(word),
+      ],
+    };
+    cardElement.addEventListener('click', clickCard);
+  }
+}
 
-// Add functions to buttons
-btnRoll.addEventListener('click', rollDice);
-btnHold.addEventListener('click', holdScore);
-btnNew.addEventListener('click', resetGame);
+// wordBank contains the code phrase, today's 20 words, and correct words
+let gameInfo = {
+  words: [
+    'castle',
+    'golfer',
+    'September',
+    'virgin',
+    'spot',
+    'still',
+    'sparkling',
+    'crawl',
+    'dust',
+    'placid',
+    'vehicles',
+    'zipper',
+    'cool',
+    'congested',
+    'pirate',
+    'true',
+    'bonkers',
+    'pop',
+    'adventure',
+    'resilient',
+  ],
+  phrases: {
+    Unfazed: ['still', 'resilient', 'cool', 'placid'],
+    'Beverage Options': ['cool', 'virgin', 'still', 'sparkling', 'pop'],
+    traffic: ['vehicles', 'crawl', 'zipper', 'congested'],
+  },
+};
+populateCardList(gameInfo);
+givePhrase(currentPhrase);
+
+btnSubmitElement.addEventListener('click', submit);
+btnDeselectElement.addEventListener('click', deselectAllCards);
+// maybe you get three words and the whole bank(?) for each one.
+//Maybe some words could be used more than once? or maybe choosing one blocks it for the next one.
